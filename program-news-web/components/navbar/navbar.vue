8bf91adade03ec348561ac1fdd3365ac2a07fc31
kimzing <kimzing@163.com>
2021-03-09 19:01:42 +0800

    完成基础搜索页

--- program-news-web/components/navbar/navbar.vue
+++ program-news-web/components/navbar/navbar.vue
@@ -2,12 +2,22 @@
 	<view>
 		<view class="navbar">
 			<view :style="{height: statusBarHeight +'px'}"></view>
-			<view class="navbar-content" :style="{width: navbarWidth + 'px', height: navbarHeight + 'px'}">
-				<view class="navbar-search">
+			<view v-if="source==='home'" class="navbar-content"
+				:style="{width: navbarWidth + 'px', height: navbarHeight + 'px'}">
+				<view class="navbar-search" @click="openSearch">
 					<view class="search-icon">
 						<uni-icons type="search" size="16" color="#999"></uni-icons>
 					</view>
 					<view class="search-text">uni-app、vue</view>
+				</view>
+			</view>
+
+			<view v-else class="navbar-content search" :style="{width: navbarWidth + 'px', height: navbarHeight + 'px'}">
+				<view class="back-icon" @click="backToHome">
+					<uni-icons type="back" size="25" color="#fff"></uni-icons>
+				</view>
+				<view class="navbar-search">
+					<input placeholder="请输入内容" maxlength="-1" @confirm="confirmSearch" focus="true"/>
 				</view>
 			</view>
 		</view>
@@ -17,6 +27,14 @@
 
 <script>
 	export default {
+		props: {
+			source: {
+				type: String,
+				default () {
+					return "home"
+				}
+			}
+		},
 		data() {
 			return {
 				statusBarHeight: 0,
@@ -31,12 +49,28 @@
 			// #ifndef H5 || APP-PLUS || MP-ALIPAY
 			let menuInfo = uni.getMenuButtonBoundingClientRect()
 			this.navbarWidth = menuInfo.left
-			this.navbarHeight = (menuInfo.bottom - systemInfo.statusBarHeight) + (menuInfo.top - systemInfo.statusBarHeight)
+			this.navbarHeight = (menuInfo.bottom - systemInfo.statusBarHeight) + (menuInfo.top - systemInfo
+				.statusBarHeight)
 			// #endif
+			
 		},
 		computed: {
 			fixHeight: function() {
 				return this.statusBarHeight + this.navbarHeight
+			}
+		},
+		methods: {
+			openSearch() {
+				console.log("123123");
+				uni.navigateTo({
+					url: "/pages/home-search/home-search"
+				})
+			},
+			confirmSearch(e) {
+				this.$emit("confirmSearch", e.detail)
+			},
+			backToHome() {
+				uni.navigateBack()
 			}
 		}
 	}
@@ -50,6 +84,7 @@
 		background-color: $main-color;
 		width: 100%;
 		z-index: 99;
+
 		.navbar-content {
 			display: flex;
 			justify-content: center;
@@ -57,6 +92,7 @@
 			height: 45px;
 			padding: 0 15px;
 			box-sizing: border-box;
+
 			.navbar-search {
 				display: flex;
 				align-items: center;
@@ -65,17 +101,33 @@
 				border-radius: 30px;
 				padding: 0 10px;
 				background-color: #FFFFFF;
-			
-				.search-icon {
-				}
-			
+
+				.search-icon {}
+
 				.search-text {
 					margin-left: 10px;
 					font-size: 12px;
 					color: #999999;
 				}
 			}
+			&.search {
+				padding-left: 0;
+				.navbar-search {
+					border-radius: 5px;
+					input {
+						width: 100%;
+					}
+				}
+				.back-icon {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					width: 30px;
+					height: 30px;
+					padding: 0 5px;
+				}
+			}
 		}
-		
+
 	}
 </style>
---
+++ program-news-web/pages/home-search/home-search.vue
@@ -0,0 +1,99 @@
+<template>
+	<view class="home">
+		<navbar source="search" @confirmSearch="confirmSearch"></navbar>
+		<view class="history">
+			<view class="history-header">
+				<view class="history-title">搜索历史</view>
+				<view class="history-clear">清空</view>
+			</view>
+			<view v-if="historyList.length > 0" class="history-footer">
+				<view v-for="item, index in historyList" :key="index" class="history-item">{{item}}</view>
+			</view>
+			<view v-else class="no-history">
+				<view> 没有搜索历史</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				historyList: ["Vue", "UniApp", "Java", "Python", "Go", "React", "Android", "IOS", "Mac"]
+				// historyList: []
+			};
+		},
+		methods: {
+			confirmSearch(searchText) {
+				console.log(searchText);
+			}
+		}
+
+	}
+</script>
+
+<style lang="scss">
+	page {
+		height: 100%;
+		display: flex;
+		background-color: #f5f5f5;
+	}
+
+	.home {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+
+		.history {
+			background-color: #fff;
+			flex-direction: column;
+
+			.history-header {
+				display: flex;
+				flex-flow: row nowrap;
+				justify-content: space-between;
+				align-items: center;
+				padding: 10px 15px;
+				font-size: 14px;
+				border-bottom: 1px #f5f5f5 solid;
+
+				.history-title {
+					color: $main-color;
+				}
+
+				.history-clear {
+					color: #30b33a;
+					font-weight: bold;
+				}
+			}
+
+			.history-footer {
+				display: flex;
+				flex-flow: row wrap;
+				padding: 15px;
+				padding-top: 0;
+
+				.history-item {
+					padding: 2px 10px;
+					margin-top: 12px;
+					margin-right: 10px;
+					border-radius: 5px;
+					border: 1px #666 solid;
+					font-size: 14px;
+					color: #666;
+				}
+			}
+
+			.no-history {
+				width: 100%;
+				height: 200px;
+				font-size: 12px;
+				color: #999;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		}
+	}
+</style>
